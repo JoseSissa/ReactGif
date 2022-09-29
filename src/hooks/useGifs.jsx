5c4861a796a983c218ea89258a60331ed0955232
Jose <jsissame3484@universidadean.edu.co>
2022-09-28 20:06:46 -0500

    Localstorage

--- src/hooks/useGifs.jsx
+++ src/hooks/useGifs.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getGifs } from '../services/getGifs';
 
+// If keyword is Null, the value will take it from localstorage
 function useGifs( keyword = localStorage.getItem('lastKeyword')) {
     const [gifs, setGifs] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -11,6 +12,7 @@ function useGifs( keyword = localStorage.getItem('lastKeyword')) {
             .then(gifs => {
                 setGifs(gifs)
                 setLoading(false)
+                // It will Store the last search in localstorage
                 localStorage.setItem('lastKeyword', keyword)
             })
     }, [keyword]);
