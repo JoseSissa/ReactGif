eed641c6a8dc2dfe4abaf287606b9318a130b512
Jose <jsissame3484@universidadean.edu.co>
2022-09-28 20:34:56 -0500

    Localstorage

--- src/hooks/useGifs.jsx
+++ src/hooks/useGifs.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { getGifs } from '../services/getGifs';
 
-// If keyword is Null, the value will take it from localstorage
-function useGifs( keyword = localStorage.getItem('lastKeyword')) {
+function useGifs( keyword ) {
+
     const [gifs, setGifs] = useState([]);
     const [loading, setLoading] = useState(false);
+    // If keyword is null or empty, the value will take it from localstorage or will be random
+    keyword = keyword || localStorage.getItem('lastKeyword') || "random";
 
     useEffect(() => {
         setLoading(true);
--- src/services/getGifs.jsx
+++ src/services/getGifs.jsx
@@ -1,6 +1,7 @@
 const apiKey = 'dIJrma20pSU6ymMwWnDbiaT7NFHeAGVa&q';
 
-const getGifs = ( {keyword = 'house of dragon'} = {}) => {
+const getGifs = ( keyword ) => {
+    console.log(keyword);
     const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${keyword}&limit=30&offset=0&rating=G&lang=en`
     return (fetch(apiURL)
         .then(res => res.json())
@@ -8,7 +9,8 @@ const getGifs = ( {keyword = 'house of dragon'} = {}) => {
         const {data} = response
         const gifs = data.map(image => {
             const { images, title, id } = image
-            const { url } = images.downsized_medium
+            console.log(image);
+            const { url } = images.downsized_still
             return { title, id, url }
         })
         return gifs
