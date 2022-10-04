615bd2e27d7409905643d41c3f115364756876e1
Jose <jsissame3484@universidadean.edu.co>
2022-10-04 14:06:25 -0500

    Interception observer

--- src/components/TrendingSearches/TrendingSearches.jsx
+++ src/components/TrendingSearches/TrendingSearches.jsx
@@ -10,6 +10,35 @@ function TrendingSearches() {
     }, []);
 
     return <Category gifs={trending} />;
-}
+};
+
+
+// Con esta función queremos que la parte de los trending se muestre y por tanto se haga la petición a la API
+// únicamente cuando la sección que tiene los trendings se muestre en pantalla, esto para evitar las una petición
+// cuando no se necesite
+// se usará la API => INTRERSECTION OBSERVER MDN
+function LazyTrending() {
+
+    const [show, setShow] = useState(false);
+    useEffect(() => {
+        const onChange = (entries) => {
+
+        };
+        const observer = new IntersectionObserver(onChange, {
+            rootMargin: '100px'
+        })
+
+        observer.observe(document.getElementById('lazyTrending'));
+
+        return 
+            (<div id='lazyTrending'>
+                {show ? <TrendingSearches /> : null}
+            </div>)
+
+    }, []);
+
+};
+
+
 
 export { TrendingSearches };
