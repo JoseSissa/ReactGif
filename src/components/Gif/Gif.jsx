3e347e0dcef3a142acd1480171f475d344985604
Jose <jsissame3484@universidadean.edu.co>
2022-10-24 14:35:05 -0500

    librería debounce

--- src/components/Gif/Gif.jsx
+++ src/components/Gif/Gif.jsx
@@ -13,4 +13,9 @@ function Gif({ id, title, url }) {
      );
 };
 
-export default React.memo(Gif);
+// React memo sirve para decirle a react que si las props no cambian, entonces no me renderices nuevamente le compoenente
+// Si dentro de las props le pasamos un objeto, array o función, va a ignorar al memo debido a que va a mirar la referenncia del objeto y no su contenido
+// Para evitar eso, hacemos una comparación de la id prop, donde si son iguales las pre y next prop significa que la porpo no ha cambiado y por lo cual no debería volver a renderizar el componente
+export default React.memo(Gif, (prevProps, nextProps) => {
+    return prevProps.id === nextProps.id;
+});
